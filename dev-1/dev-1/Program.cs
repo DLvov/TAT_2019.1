99811a3093ae33f540820439603337cefa763c47
DLvov <dim.lvov2010@gmail.com>
2019-03-07 01:58:15 +0300

    first task with comments

---
+++ dev-1/dev-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dev_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Arguments from command line</param>
+        static void Main(string[] args)
+        {
+            subsequenceCollector myCollector = new subsequenceCollector();
+            myCollector.collect(args[0]);
+            myCollector.showSubsequences();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dev-1/dev-1/subsequenceCollector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dev_1
+{
+    /// <summary>
+    /// This class collect the subsequences and show them
+    /// </summary>
+    class subsequenceCollector
+    {
+        string[] subsequences = { };
+        /// <summary>
+        /// This method collects substrings into array subsequences
+        /// </summary>
+        public void collect(string sequence)
+        {
+            if (sequence.Length < 2)
+            {
+                Console.WriteLine("Your string is incorrect! (Length < 2)");
+                return;
+            }
+            for (int i = 0; i < sequence.Length - 1; i++)
+                for (int j = i; j < sequence.Length - 1; j++)
+                {
+                    if (sequence[j] == sequence[j + 1])
+                        break;
+                    string subsequence = sequence.Substring(i, j - i + 2);
+                    Array.Resize(ref subsequences, subsequences.Length + 1);
+                    subsequences[subsequences.Length - 1] = subsequence;
+                }
+        }
+        /// <summary>
+        /// This method shows the array with subsequences
+        /// </summary>
+        public void showSubsequences()
+        {
+            for (int i = 0; i < subsequences.Length; i++)
+                Console.WriteLine(subsequences[i]);
+        }
+    }
+}
