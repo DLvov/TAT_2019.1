ca31f7ddfae1bbae0f0b5fa9ab0208a05e76820e
DLvov <dim.lvov2010@gmail.com>
2019-03-16 11:19:37 +0300

    Letter status checker not-optimized

---
+++ dev-2/dev-2/PhonemeBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace dev_2
+{
+    class PhonemeBuilder
+    {
+        public string BuildPhoneme()
+        {
+            var phoneme = new StringBuilder();
+            return phoneme.ToString();
+        }
+    }
+}
