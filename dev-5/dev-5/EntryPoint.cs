1794246936a93f7d074cc9cd7842389272ab3d2f
DLvov <dim.lvov2010@gmail.com>
2019-03-31 17:56:58 +0300

    finish entry point

--- dev-5/dev-5/EntryPoint.cs
+++ dev-5/dev-5/EntryPoint.cs
@@ -12,12 +12,32 @@ namespace dev_5
     class EntryPoint
     {
         /// <summary>
-        /// 
+        /// This programm :
+        ///     creates objects: Bird, Plane, SpaceShip
+        ///     calls methods FlyTo() and GetFlyTime() for each one
+        ///     and add flight times into list results
         /// </summary>
         /// <param name="args">Arguments from command line</param>
         static void Main(string[] args)
         {
-            
+            try
+            {
+                var destinationPoint = new Point(100, 200, 800);
+                var flyableEntities = new IFlyable[3];
+                flyableEntities[0] = new Bird();
+                flyableEntities[1] = new Plane();
+                flyableEntities[2] = new SpaceShip();
+                var results = new List<double>();
+                foreach (var item in flyableEntities)
+                {
+                    item.FlyTo(destinationPoint);
+                    results.Add(item.GetFlyTime());
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something wrong!");
+            }
         }
     }
 }
