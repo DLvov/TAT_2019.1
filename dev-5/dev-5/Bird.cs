47f0b4543358b98a448cf70879dde95047a945a2
DLvov <dim.lvov2010@gmail.com>
2019-03-31 12:46:26 +0300

    comments for bird and plane

--- dev-5/dev-5/Bird.cs
+++ dev-5/dev-5/Bird.cs
@@ -2,17 +2,28 @@
 
 namespace dev_5
 {
+    /// <summary>
+    /// This class corresponds to the bird.
+    /// Speed for bird will be measured in km / h.
+    /// </summary>
     class Bird : FlyingEntity
     {
+        /// <summary>
+        /// This constructor inicialize Name.
+        /// </summary>
         public Bird()
         {
             Name = "Bird"; 
         }
 
+        /// <summary>
+        /// This method returns bird flight time at random speed in the range of (0-20).
+        /// </summary>
+        /// <returns>Time of the flight</returns>
         public override double GetFlyTime()
         {
             var random = new Random();
-            Speed = random.Next(0, 20); // km/h
+            Speed = random.Next(0, 20);
             return currentPoint.getDistance(prevPoint) / Speed;
         }
     }
--- dev-5/dev-5/Plane.cs
+++ dev-5/dev-5/Plane.cs
@@ -1,21 +1,33 @@
 ﻿namespace dev_5
 {
+    /// <summary>
+    /// This class corresponds to the plane.
+    /// Speed for plane will be measured in km / h.
+    /// </summary>
     class Plane : FlyingEntity
     {
-        private const int INITIAL_SPEED = 200; // km/h
+        private const int INITIAL_SPEED = 200;
 
+        /// <summary>
+        /// This constructor inicialize name and speed.
+        /// </summary>
         public Plane()
         {
             Name = "Plane";
             Speed = INITIAL_SPEED;
         }
 
+        /// <summary>
+        /// This method returns plane flight time. Plane speed increases by 10 km/h every 10 km.
+        /// After the flight the speed of the plane becomes the initial.
+        /// </summary>
+        /// <returns>Time of the flight</returns>
         public override double GetFlyTime()
         {
             double time = 0;
             double remainingDistance = currentPoint.getDistance(prevPoint);
             int acceleration = 10;
-            int accelerationDistance = 10; // speed raising each 10 km
+            int accelerationDistance = 10;
 
             while(remainingDistance > 0)
             {
