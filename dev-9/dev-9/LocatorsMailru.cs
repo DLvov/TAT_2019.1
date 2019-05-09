dce696910bcd725d319485752eb806c4f40e4198
DLvov <dim.lvov2010@gmail.com>
2019-05-09 22:48:11 +0300

    adding locators and page object

--- dev-9/dev-9/LocatorsMailru.cs
+++ dev-9/dev-9/LocatorsMailru.cs
@@ -8,5 +8,28 @@
             public string PasswordLocator { get; } = "//input[@id = 'mailbox:password']";
             public string SubmitButtonLocator { get; } = "//input[@class = 'o-control']";
         }
+
+        public class InboxPageLocators
+        {
+            public string NewLetterButtonLocator { get; } = "//a[@class = 'b-toolbar__btn b-toolbar__btn_with-foldings js-shortcut']";
+            public string LastInboxLetterLocator { get; } = "//div[@class = 'b-datalist__item__body']";
+            public string MessageContentLocator { get; } = "//div[@class = 'class_1557437996']";
+            public string ProfileLocator { get; } = "//i[@id = 'PH_user-email']";
+            public string SettingsButtonLocator { get; } = "//a[@class = 'x-ph__menu__dropdown_auth__links__link']";
+        }
+
+        public class NewLetterPageLocator
+        {
+            public string RecipientLocator { get; } = "//textarea[@tabindex='4']";
+            public string SubjectLocator { get; } = "//input[@class = 'b-input']";
+            public string TextLocator { get; } = "//body[@id = 'tinymce']";
+            public string SendMessageButtonLocator { get; } = "//div[@data-bem = 'b-toolbar__btn']";
+        }
+
+        public class SettingsPageLocator
+        {
+            public string NickNameLocator { get; } = "//input[@name = 'NickName']";
+            public string SaveButtonlocator { get; } = "//button[@class = 'btn btn_main btn_stylish']";
+        }
     }
 }
--- dev-9/dev-9/LocatorsProtonmail.cs
+++ dev-9/dev-9/LocatorsProtonmail.cs
@@ -8,5 +8,27 @@
             public string PasswordLocator { get; } = "//input[@id = 'password']";
             public string SubmitButtonLocator { get; } = "//input[@id = 'login_btn']";
         }
+
+        public class InboxPageLocators
+        {
+            public string NewLetterButtonLocator { get; } = "//button[@class contains() = 'sidebar-btn-compose']";
+            public string LastInboxLetterLocator { get; } = "//div[@class = 'conversation-meta']";
+            public string MessageContentLocator { get; } = "//div[@class = 'bodyDecrypted email message-body-container']";
+            public string SettingsButtonLocator { get; } = "//a[@id = 'tour-settings']";
+        }
+
+        public class NewLetterPageLocator
+        {
+            public string RecipientLocator { get; } = "//input[@class = 'autocompleteEmails-input no-outline']";
+            public string SubjectLocator { get; } = "//input[@ng-model = 'message.Subject']";
+            public string TextLocator { get; } = "//html[@class = 'c']/body";
+            public string SendMessageButtonLocator { get; } = "//button[@class = 'pm_button primary mobileFull composer-btn-send btnSendMessage-btn-action']";
+        }
+
+        public class SettingsPageLocator
+        {
+            public string NickNameLocator { get; } = "//input[@id = 'displayName']";
+            public string SaveButtonlocator { get; } = "//button[@class = 'pm_button primary']";
+        }
     }
 }
